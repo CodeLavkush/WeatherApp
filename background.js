5bb5ed3f63f47e61eaa4c7f04c2f6930cefb3265
CodeLavkush <mauryalavkush19@gmail.com>
2024-09-02 18:20:16 +0530

    Initial commit

---
+++ background.js
@@ -0,0 +1,20 @@
+let bgArr = ["img/rain_1.gif","img/rain_2.gif","img/rain_3.gif","img/rain_4.gif","img/rain_5.gif","img/rain_6.gif"]
+
+let currentIndex = 0;
+
+setInterval(()=>{
+    changeBg();
+    currentIndex++;
+}, 4000)
+
+function changeBg(){
+    let searchBg = document.querySelector(".search-section");
+
+    if(currentIndex < bgArr.length){
+        searchBg.style.backgroundImage = `url(${bgArr[currentIndex]})`;
+    }
+    else{
+        currentIndex = 0;
+        searchBg.style.backgroundImage = `url(${bgArr[currentIndex]})`;
+    }
+}
---
+++ script.js
@@ -0,0 +1,144 @@
+
+// getting the api key
+const API_KEY = "3bac92471883fbe40bf7f0f9fd292068";
+
+// getting the search button and calling the event
+let searchBtn = document.querySelector(".search-icon");
+searchBtn.addEventListener('click',()=>{
+    let searchResult = document.querySelector(".search-result");
+    let searchBox = document.querySelector(".search-box");
+    let featuredCities = document.querySelector(".featured-cities");
+
+    searchResult.style.display = "flex";
+    featuredCities.style.display = "none";
+    getCityWeather(searchBox.value);
+})
+
+// images icon object
+const iconObj = {
+    wind: "img/windspeed_icon.svg",
+    temp: "img/tempreture_icon.svg",
+    humidity: "img/humidity_icon.svg"
+}
+
+// search city fuction
+async function getCityWeather(cityname) {
+    let searchResult = document.querySelector(".search-result");
+    let data = await getWeatherData(API_KEY, cityname);
+    let id = await weatherId(cityname);
+
+    let cityCard = `<div class="city-card">
+                <div class="top">
+                    <img src="https://openweathermap.org/img/wn/${id}@2x.png" alt="weathericon">
+                </div>
+                <div class="bottom">
+                    <div class="upperSide">
+                        <h3 class="cityName">${data.name}</h3>
+                    </div>
+                    <div class="downSide">
+                        <div class="temp">
+                            <img src="${iconObj.temp}" alt="temp">
+                            <p>${getTemp(data.main.temp)} °C</p>
+                        </div>
+                        <div class="wind-speed">
+                            <img src="${iconObj.wind}" alt="speed">
+                            <p>${data.wind.speed} m/s</p>
+                        </div>
+                        <div class="humidity">
+                            <img src="${iconObj.humidity}" alt="humidity">
+                            <p>${data.main.humidity}%</p>
+                    </div>
+                </div>
+            </div>`
+    searchResult.innerHTML = cityCard;
+}
+
+// getting the weather data
+async function getWeatherData(apikey, cityname){
+    try{
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityname.toLowerCase()}&appid=${apikey}`);
+        
+        if(!response.ok){
+            throw new Error("Failed to Fetch data!");
+        }
+        
+        return await response.json();;
+    }
+    catch(error){
+        console.error(error);
+    }
+}
+
+// creating featured cards
+async function cardCreation(cityname) {
+
+    let citiesLists = document.querySelector(".cities-list");
+    let data = await getWeatherData(API_KEY, cityname);
+    let id = await weatherId(cityname);
+
+    let cityCard = `<div class="city-card">
+                <div class="top">
+                    <img src="https://openweathermap.org/img/wn/${id}@2x.png" alt="weathericon">
+                </div>
+                <div class="bottom">
+                    <div class="upperSide">
+                        <h3 class="cityName">${data.name}</h3>
+                    </div>
+                    <div class="downSide">
+                        <div class="temp">
+                            <img src="${iconObj.temp}" alt="temp">
+                            <p>${getTemp(data.main.temp)} °C</p>
+                        </div>
+                        <div class="wind-speed">
+                            <img src="${iconObj.wind}" alt="speed">
+                            <p>${data.wind.speed} m/s</p>
+                        </div>
+                        <div class="humidity">
+                            <img src="${iconObj.humidity}" alt="humidity">
+                            <p>${data.main.humidity}%</p>
+                    </div>
+                </div>
+            </div>`
+    citiesLists.innerHTML += cityCard;
+}
+
+// weather id handing
+let weatherId = async (cityname)=>{
+    let data = await getWeatherData(API_KEY, cityname);
+    let condition = data.weather[0].main;
+    if(condition === "thuderstrom"){
+        return data.weather[0].icon;
+    }
+    else if(condition === "Dizzle"){
+        return data.weather[0].icon;
+    }
+    else if(condition === "Rain"){
+        return data.weather[0].icon;
+    }
+    else if(condition === "Snow"){
+        return data.weather[0].icon;
+    }
+    else if(condition === "clear"){
+        return data.weather[0].icon;
+    }
+    else if(condition === "Clouds"){
+        return data.weather[0].icon;
+    }
+    else{
+        return data.weather[0].icon;
+    }
+}
+
+// converting tempreture from kalvin to celsius
+function getTemp(kalvin){
+    return Math.floor(kalvin - 273.15);
+}
+
+cardCreation("delhi");
+cardCreation("maharashtra")
+cardCreation("mumbai")
+cardCreation("goa")
+cardCreation("bangalore")
+cardCreation("rajasthan")
+cardCreation("uttar pradesh")
+cardCreation("west bengal")
