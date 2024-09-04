c7b7ca96a716e13294dc67b14969b2f1ccd9d759
CodeLavkush <mauryalavkush19@gmail.com>
2024-09-04 23:18:54 +0530

    solved TypeError: res.sendFile is not a function in index.js

--- index.js
+++ index.js
@@ -9,16 +9,12 @@ const port = process.env.PORT || 10000;
 
 app.use(cors()); // Enable CORS
 
-// Define a function to handle file requests efficiently
-function sendFile(req, res, filename) {
-    res.sendFile(filename, { root: __dirname });
-}
 
 // Use the sendFile function for all file requests
-app.get('/', sendFile.bind(null, './index.html'));
-app.get('/style.css', sendFile.bind(null, './style.css'));
-app.get('/script.js', sendFile.bind(null, './script.js'));
-app.get('/background.js', sendFile.bind(null, './background.js'));
+app.get('/', sendFileToEndPoint.bind(null, './index.html'));
+app.get('/style.css', sendFileToEndPoint.bind(null, './style.css'));
+app.get('/script.js', sendFileToEndPoint.bind(null, './script.js'));
+app.get('/background.js', sendFileToEndPoint.bind(null, './background.js'));
 
 // Use a loop to handle image requests more concisely
 const imageFiles = [
@@ -35,8 +31,12 @@ const imageFiles = [
     'windspeed_icon.svg'
 ];
 imageFiles.forEach((file) => {
-    app.get(`/img/${file}`, sendFile.bind(null, `./img/${file}`));
+    app.get(`/img/${file}`, sendFileToEndPoint.bind(null, `./img/${file}`));
 });
+// Define a function to handle file requests efficiently
+function sendFileToEndPoint(req, res, filename) {
+    res.sendFile(filename, { root: __dirname });
+}
 
 
 app.get('/api/apikey/WeatherAPI_KEY', (req, res) => {
