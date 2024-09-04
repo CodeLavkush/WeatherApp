e6ef3e1179a1c5d6c221ca96445ca3e7a2760376
CodeLavkush <mauryalavkush19@gmail.com>
2024-09-04 23:21:35 +0530

    revert back the changes

--- index.js
+++ index.js
@@ -9,35 +9,52 @@ const port = process.env.PORT || 10000;
 
 app.use(cors()); // Enable CORS
 
-
-// Use the sendFile function for all file requests
-app.get('/', sendFileToEndPoint.bind(null, './index.html'));
-app.get('/style.css', sendFileToEndPoint.bind(null, './style.css'));
-app.get('/script.js', sendFileToEndPoint.bind(null, './script.js'));
-app.get('/background.js', sendFileToEndPoint.bind(null, './background.js'));
-
-// Use a loop to handle image requests more concisely
-const imageFiles = [
-    'github.svg',
-    'humidity_icon.svg',
-    'rain_1.gif',
-    'rain_2.gif',
-    'rain_3.gif',
-    'rain_4.gif',
-    'rain_5.gif',
-    'rain_6.gif',
-    'Search_icon.svg',
-    'tempreture_icon.svg',
-    'windspeed_icon.svg'
-];
-imageFiles.forEach((file) => {
-    app.get(`/img/${file}`, sendFileToEndPoint.bind(null, `./img/${file}`));
+app.get('/', (req, res) => {
+    res.sendFile("./index.html", { root: __dirname });
 });
-// Define a function to handle file requests efficiently
-function sendFileToEndPoint(req, res, filename) {
-    res.sendFile(filename, { root: __dirname });
-}
+app.get('/style.css', (req, res) => {
+    res.sendFile("./style.css", { root: __dirname });
+});
+app.get('/script.js', (req, res) => {
+    res.sendFile("./script.js", { root: __dirname });
+});
+app.get('/background.js', (req, res) => {
+    res.sendFile("./background.js", { root: __dirname });
+});
 
+app.get('/img/github.svg', (req, res) => {
+    res.sendFile("./img/github.svg", { root: __dirname });
+});
+app.get('/img/humidity_icon.svg', (req, res) => {
+    res.sendFile("./img/humidity_icon.svg", { root: __dirname });
+});
+app.get('/img/rain_1.gif', (req, res) => {
+    res.sendFile("./img/rain_1.gif", { root: __dirname });
+});
+app.get('/img/rain_2.gif', (req, res) => {
+    res.sendFile("./img/rain_2.gif", { root: __dirname });
+});
+app.get('/img/rain_3.gif', (req, res) => {
+    res.sendFile("./img/rain_3.gif", { root: __dirname });
+});
+app.get('/img/rain_4.gif', (req, res) => {
+    res.sendFile("./img/rain_4.gif", { root: __dirname });
+});
+app.get('/img/rain_5.gif', (req, res) => {
+    res.sendFile("./img/rain_5.gif", { root: __dirname });
+});
+app.get('/img/rain_6.gif', (req, res) => {
+    res.sendFile("./img/rain_6.gif", { root: __dirname });
+});
+app.get('/img/Search_icon.svg', (req, res) => {
+    res.sendFile("./img/Search_icon.svg", { root: __dirname });
+});
+app.get('/img/tempreture_icon.svg', (req, res) => {
+    res.sendFile("./img/tempreture_icon.svg", { root: __dirname });
+});
+app.get('/img/windspeed_icon.svg', (req, res) => {
+    res.sendFile("./img/windspeed_icon.svg", { root: __dirname });
+});
 
 app.get('/api/apikey/WeatherAPI_KEY', (req, res) => {
     res.json({ key: process.env.WEATHER_API_KEY });
